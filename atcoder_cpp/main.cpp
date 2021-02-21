bd9cfe69b926ced6b63133b8148b6e25cbd1ff44
Akifumi Abe <akiabe0831@gmail.com>
2021-02-21 18:26:53 +0900

    first commit

--- atcoder_cpp/main.cpp
+++ atcoder_cpp/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  atcoder_cpp
-//
-//  Created by 阿部 昌史 on 2021/02/21.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    cout << "Hello" << endl;
+    
 }
